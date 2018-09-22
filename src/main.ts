076e2050110ef650348839f92bb4011f63402655
Eric Lifka <eric.lifka@gmail.com>
2018-09-22 10:06:23 -0400

    event listeners to pause game when it's not focused

--- src/main.ts
+++ src/main.ts
@@ -44,3 +44,18 @@ const runLoop = new RunLoop(function (deltaT) {
 });
 
 runLoop.start();
+
+document.addEventListener("visibilitychange", function () {
+    if (document.hidden) {
+        runLoop.stop();
+    }
+});
+
+window.addEventListener("blur", function () {
+    runLoop.stop();
+});
+
+window.addEventListener("focus", function () {
+    // inputs.forEach(input => input.clearState());
+    runLoop.start();
+});
