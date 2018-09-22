a1534357cfe6fd7e29718a3fd44cb1190a156006
Eric Lifka <eric.lifka@gmail.com>
2018-09-22 09:40:06 -0400

    take a more robust run-loop from a previous project

--- src/run-loop.ts
+++ src/run-loop.ts
@@ -1,40 +1,92 @@
+let fpsCounterDOM = null;
+function updateFPScounter(dtime) {
+    if (!fpsCounterDOM) {
+        fpsCounterDOM = document.createElement('div');
+        fpsCounterDOM.classList.add('fps-counter');
+        fpsCounterDOM.oldfps = 0;
+        document.body.appendChild(fpsCounterDOM);
+    }
+
+    let fps = Math.floor(1000 / dtime * 10) / 10;
+    fpsCounterDOM.oldfps = fps;
+    let fpsStr = `${fps}`;
+    if (fpsStr.length <= 2) {
+        fpsStr = `${fpsStr}.0`;
+    }
+    fpsCounterDOM.innerHTML = fpsStr;
+}
+
+class FpsTracker {
+    frameTimes: number[] = [];
+    totalTime: number = 20 * 100;
+
+    constructor() {
+        for (let i = 0; i < 100; i++) {
+            this.frameTimes.push(20);
+        }
+    }
+
+    push(ftime: number): void {
+        let overflow = this.frameTimes.shift();
+        this.totalTime += ftime - overflow;
+        this.frameTimes.push(ftime);
+    }
+
+    average(): number {
+        return this.totalTime / 100;
+    }
+}
 
 export default class RunLoop {
+    fpsTracker: FpsTracker = new FpsTracker();
+    active: boolean = false;
+    lastFrameTime: number = Date.now();
+    boundFrameHandler: (time: number) => void;
+    frameCount: number = 0;
 
-    readonly callback: (deltaT: number) => void;
-    private halt: boolean;
-    private lastTime: number;
+    constructor(public callback: Function = (() => null)) {
+        this.boundFrameHandler = this.frameHandler.bind(this);
+    }
 
-    public constructor(callback: (deltaT: number) => void) {
+    frameHandler(time: number): void {
+        if (!this.active) return;
+
+        let currentTime = Date.now();
+        let dtime = currentTime - this.lastFrameTime;
+
+        this.lastFrameTime = currentTime;
+        this.updateFPScounter(dtime);
+
+        try {
+            this.callback(dtime);
+        } catch (e) {
+            console.error('Error running frame: ', e);
+        }
+
+        window.requestAnimationFrame(this.boundFrameHandler);
+    }
+
+    start(): void {
+        if (!this.active) {
+            this.active = true;
+            window.requestAnimationFrame(this.boundFrameHandler);
+        }
+    }
+
+    stop(): void {
+        this.active = false;
+    }
+
+    setCallback(callback: (dtime: number) => void) {
         this.callback = callback;
     }
 
-    public start(): void {
-        this.halt = false;
-        this.loop();
-    }
+    updateFPScounter(dtime) {
+        this.fpsTracker.push(dtime);
 
-    public stop(): void {
-        this.halt = true;
-    }
-
-    private loop(): void {
-        if (this.halt) {
-            return;
+        if (this.frameCount++ > 60) {
+            updateFPScounter(this.fpsTracker.average());
+            this.frameCount = 0;
         }
-
-        window.requestAnimationFrame((time: number): void => {
-            let deltaT = time - this.lastTime;
-            this.lastTime = time;
-
-            try {
-                this.callback(deltaT);
-            }
-            catch (e) {
-                console.error("Error in frame:", e);
-            }
-
-            this.loop();
-        });
     }
 }
